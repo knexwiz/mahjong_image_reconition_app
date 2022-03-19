f4b5de35559a6598b0ad9106142f9c156079a369
NevinAF <nafoste2@asu.edu>
2022-03-19 16:52:15 -0700

    Added Template Matching, Hist comp, tile approx

--- ImageRecognision/ImageClassificationTest.py
+++ ImageRecognision/ImageClassificationTest.py
@@ -1,11 +1,14 @@
+from re import I
 import cv2
 import os
 import numpy as np
+import random
 import VideoCaptureHelper
 from ImgDescription import ImageDescription
+import ImgDescription
 
-TILE_SIZE = (90,120)
-ORB_CLASSIFIER_PATH = "./ImageClassifiers"
+TILE_SIZE = (75,100)
+ORB_CLASSIFIER_PATH = "../mahjong_img"
 
 cap = VideoCaptureHelper.createVideoCapture(camerano=1)
 
@@ -23,6 +26,8 @@ def CreateImgDictionary(path) -> list[ImageDescription]:
 		if img is None:
 			continue
 
+		img = cv2.resize(img, TILE_SIZE)
+
 		filename = os.path.splitext(dir)[0]
 		imgDesc = ImageDescription(img, filename)
 
@@ -35,40 +40,138 @@ def CreateImgDictionary(path) -> list[ImageDescription]:
 	return dictionary
 
 imgdict = CreateImgDictionary(ORB_CLASSIFIER_PATH)
+cv2.imshow("Image", imgdict[0].sourceImage)
 
 while True:
 	success, inputImage = cap.read()
 
-	inputDesc = ImageDescription(inputImage)
+	inputDesc = ImageDescription(inputImage.copy())
 	inputDesc.CreateORBDescription()
 	inputDesc.CreateHistogramDescription()
 
-	matches = inputDesc.GetORBMatches(imgdict)
-
-	id = 0
-	while id < 5 and matches[id].ORBFitness > 10:
-		cv2.putText(inputImage, "%d -> %s :: %.2f" %(id, matches[id].name, matches[id].ORBFitness), \
-			(50,50 + 30 * id), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0,0,255), 2)
-		id += 1
-	if id == 0:
-		cv2.putText(inputImage, "N/A", (50 + 30 * id,50), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0,0,255), 2)
-
 	inputDesc.CreateContourDescription()
-	inputDesc.FilterContoursByArea((100, TILE_SIZE[0] * TILE_SIZE[1]))
+
+	# inputDesc.FilterContoursByArea((100, TILE_SIZE[0] * TILE_SIZE[1]))
+	inputDesc.FilterContoursByBoundsSize( (24, 24), (TILE_SIZE[1], TILE_SIZE[1]))
 	inputDesc.FilterContoursByBoarder()
-	cv2.drawContours(inputImage, inputDesc.contours, -1, (0,255,0), 1)
 
-	if not matches[0].goods is None:
-		temp1 = cv2.drawMatchesKnn(matches[0].grayImage, matches[0].kp, \
-			inputDesc.grayImage, inputDesc.kp, matches[0].goods, None, flags=2)
+	for source in imgdict:
+		color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
+		res = cv2.matchTemplate(inputDesc.grayImage,source.grayImage,cv2.TM_CCOEFF_NORMED)
+		threshold = 0.5
+		loc = np.where( res >= threshold)
+		pts = []
+		for p in zip(*loc[::-1]): pts.append(p)
+		radius = 20
+		for i in range(len(pts)):
+			found = False
+			for j in range(i+1, len(pts)):
+				if ImgDescription.pointDistance(pts[i],pts[j]) < radius:
+					found = True
+			
+			if found: continue
 
-	# cv2.rectangle(inputImage, (0,0), tilesize, (0,255,255), 3)
+			pt = pts[i]
+				
+			# string = "%.2f" %(res)
+			cv2.putText(inputImage, source.name, (pt[0],pt[1]-5), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, color, 2)
+			cv2.rectangle(inputImage, pt, (pt[0] + source.imgWidth, pt[1] + source.imgHeight), color, 2)
 
-	outputImage = np.concatenate((inputImage, temp1), axis=1)
+
+	# usedPoints = []
+	# minRadius = (int)(TILE_SIZE[0]/2)
+	# for i in range(len(inputDesc.contours)):
+	# 	irect = cv2.boundingRect(inputDesc.contours[i])
+
+	# 	if inputDesc.filtered_contours[i]: continue
+
+	# 	color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
+
+	# 	irect = ImgDescription.scaleRect(irect, TILE_SIZE)
+
+	# 	j = i
+	# 	while j+1 < len(inputDesc.contours):
+	# 		j += 1
+	# 		if inputDesc.filtered_contours[j]:
+	# 			continue
+
+	# 		jrect = cv2.boundingRect(inputDesc.contours[j])
+	# 		if ImgDescription.hasIntersection( irect, jrect ):
+	# 			inputDesc.filtered_contours[j] = True
+	# 			irect = ImgDescription.union(irect, jrect)
+
+	# 			irect = ImgDescription.scaleRect(irect, TILE_SIZE)
+
+	# 			(x, y, w, h) = jrect
+
+	# 			cv2.rectangle(inputImage, (x,y), (x+w,y+h), color, 1)
+
+	# 			j = i
+
+	# 	# cv2.drawContours(inputImage, inputDesc.contours, i, color, size)
+
+	# 	(x, y, w, h) = irect
+	# 	print (irect)
+	# 	print (len(inputDesc.sourceImage))
+
+	# 	midpoint = ( (x+w)/2, (y+h)/2 )
+	# 	for p in usedPoints:
+	# 		if ImgDescription.pointDistance(p, midpoint) < minRadius:
+	# 			continue
+	# 	usedPoints.append( midpoint )
+	# 	cv2.rectangle(inputImage, (x,y), (x+w,y+h), color, 3)
+
+		
+	# 	cp = inputDesc.sourceImage[y:y+h,x:x+w]
+
+		
+	# 	if len(cp) < 5: continue
+
+	# 	cropped = ImageDescription(cp)
+	# 	cropped.CreateORBDescription()
+	# 	cropped.CreateHistogramDescription()
+
+	# 	matches = cropped.GetORBMatches(imgdict)
+	# 	vals = cropped.GetHistMatches(imgdict)
+
+	# 	string = "N/A"
+	# 	if not (matches[0].goods is None or vals[0] is None):
+	# 		# string = "%s -> %.2f" %(matches[0].name, matches[0].ORBFitness)
+	# 		string = "%s -> %.2f" %(vals[0].name, vals[0].HistFitness)
+	# 		temp1 = cv2.drawMatchesKnn(matches[0].grayImage, matches[0].kp, \
+	# 			cropped.grayImage, cropped.kp, matches[0].goods, None, flags=2)
+			
+	# 		cv2.imshow("Cropped %d.1" %(len(usedPoints)), vals[0].hsvImage)
+	# 		cv2.imshow("Cropped %d.2" %(len(usedPoints)), cropped.hsvImage)
+			
+	# 	cv2.putText(inputImage, string, (x,y-5), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, color, 2)
+
+
+	# matches = inputDesc.GetORBMatches(imgdict)
+
+	# id = 0
+	# while id < 5 and matches[id].ORBFitness > 10:
+	# 	cv2.putText(inputImage, "%d -> %s :: %.2f" %(id, matches[id].name, matches[id].ORBFitness), \
+	# 		(50,50 + 30 * id), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0,0,255), 2)
+	# 	id += 1
+	# if id == 0:
+	# 	cv2.putText(inputImage, "N/A", (50 + 30 * id,50), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0,0,255), 2)
+
+	# if not matches[0].goods is None:
+	# 	temp1 = cv2.drawMatchesKnn(matches[0].grayImage, matches[0].kp, \
+	# 		inputDesc.grayImage, inputDesc.kp, matches[0].goods, None, flags=2)
+
+	cv2.rectangle(inputImage, (0,0), TILE_SIZE, (0,255,255), 3)
+
+	# outputImage = np.concatenate((inputImage, temp1), axis=1)
 	temp = cv2.cvtColor(inputDesc.threshImage, cv2.COLOR_GRAY2BGR)
-	temp = np.concatenate((temp, temp1), axis=1)
-	outputImage = np.concatenate((outputImage, temp), axis=0)
+	# temp = np.concatenate((temp, temp1), axis=1)
+	outputImage = np.concatenate((inputImage, temp), axis=0)
 	cv2.imshow("Result", outputImage)
-	
-	if cv2.waitKey(1) & 0xFF == ord('q'):
+
+	if cv2.waitKey(0) & 0xFF == ord('q'):
 		break
+
+cap.release()
+cv2.destroyWindow("Result")
+exit(0)
--- ImageRecognision/ImgDescription.py
+++ ImageRecognision/ImgDescription.py
@@ -1,18 +1,20 @@
 from __future__ import annotations
 import cv2
+import math
 
 CONTOUR_BLUR = 5
 CONTOUR_BOARDER_PADDING = 5
-ORB_DESCRIPTION = cv2.ORB_create(nfeatures=2000)
+ORB_DESCRIPTION = cv2.ORB_create(nfeatures=5000)
 BEST_FIT = cv2.BFMatcher()
-ORB_ALPHA = 0.3
-HIST_ALPHA = 0.3
+ORB_ALPHA = 1.0
+HIST_ALPHA = 1.0
 
 class ImageDescription:
 	def __init__(self, image, name="N/A") -> None:
 		self.name = name
 		self.sourceImage = image
 		self.grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+		self.hsvImage = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 		self.ORBFitness = (float)(0)
 		self.HistFitness = (float)(0)
 		self.imgWidth = len(image[0,:])
@@ -26,43 +28,47 @@ class ImageDescription:
 		h_bins = 50
 		s_bins = 60
 		histSize = [h_bins, s_bins]
+		
+		# hue varies from 0 to 179, saturation from 0 to 255
 		h_ranges = [0, 180]
 		s_ranges = [0, 256]
-		ranges = h_ranges + s_ranges
+		ranges = h_ranges + s_ranges # concat lists
+		
+		# Use the 0-th and 1-st channels
 		channels = [0, 1]
 
-		self.hist = cv2.calcHist( \
-			self.grayImage, channels, None, histSize, ranges, accumulate=False)
+		self.hist = cv2.calcHist([self.hsvImage], channels, None, histSize, ranges, accumulate=False)
 		cv2.normalize(self.hist, self.hist, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)
 
 	def CreateContourDescription(self) -> None:
 		self.threshImage = cv2.adaptiveThreshold(cv2.medianBlur(self.grayImage, CONTOUR_BLUR), \
 			255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-		self.contours, self.heirachy = cv2.findContours(self.threshImage, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+		self.contours, self.heirachy = cv2.findContours(self.threshImage, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+		self.filtered_contours = [False] * len(self.contours)
 
 	def FilterContoursByBoarder(self) -> None:
-		filteredContours = []
-		for c in self.contours:
-			(x, y, h, w) = cv2.boundingRect(c)
-			if x > 5 and y > 5 and x+w < self.imgWidth - 5 and y+h < self.imgHeight - 5:
-				filteredContours.append(c)
-		self.contours = filteredContours
+		i = 0
+		for i in range(len(self.contours)):
+			if self.filtered_contours[i]: continue
+			(x, y, h, w) = cv2.boundingRect(self.contours[i])
+			if not (x > 5 and y > 5 and x+w < self.imgWidth - 5 and y+h < self.imgHeight - 5):
+				self.filtered_contours[i] = True
 
 	def FilterContoursByBoundsSize(self, min :list[2], max :list[2]) -> None:
-		filteredContours = []
-		for c in self.contours:
-			(x, y, h, w) = cv2.boundingRect(c)
-			if w >= min[0] and w <= max[0] and h >= min[1] and h <= max[1]:
-				filteredContours.append(c)
-		self.contours = filteredContours
+		i = 0
+		for i in range(len(self.contours)):
+			if self.filtered_contours[i]: continue
+			(x, y, h, w) = cv2.boundingRect(self.contours[i])
+			if not (w >= min[0] and w <= max[0] and h >= min[1] and h <= max[1]):
+				self.filtered_contours[i] = True
 
 	def FilterContoursByArea(self, area :list[2]) -> None:
-		filteredContours = []
-		for c in self.contours:
-			carea = cv2.contourArea(c)
-			if carea > area[0] and carea < area[1]:
-				filteredContours.append(c)
-		self.contours = filteredContours
+		i = 0
+		for i in range(len(self.contours)):
+			if self.filtered_contours[i]: continue
+			carea = cv2.contourArea(self.contours[i])
+			if not (carea > area[0] and carea < area[1]):
+				self.filtered_contours[i] = True
 
 	def CompareORBDescription(self, other :ImageDescription) -> list:
 		try:
@@ -91,15 +97,73 @@ class ImageDescription:
 		return ImageDescription.GetORBMatches(descriptors, self)[0]
 
 	def CompareHistDescription(self, other :ImageDescription) -> float:
-		cv2.compareHist(self.hist, other.hist, cv2.HISTCMP_CORREL)
-		return 0.0
+		return cv2.compareHist(self.hist, other.hist, cv2.HISTCMP_CORREL)
 
 	def GetHistMatches(self, descriptors :list[ImageDescription]) -> list[ImageDescription]:
 		valid_matches = []
 		for source in descriptors:
-			fit = (source.HistFitness * (1 - HIST_ALPHA)) + \
+			source.HistFitness = (source.HistFitness * (1 - HIST_ALPHA)) + \
 				(source.CompareHistDescription(self) * HIST_ALPHA)
-			valid_matches.append( fit )
+			valid_matches.append( source )
+			print(source.HistFitness)
 
-		valid_matches.sort(reverse=True)
+		valid_matches.sort(key=lambda desc: desc.HistFitness ,reverse=True)
 		return valid_matches
+
+	'''
+	Can only be used when comparing images of the same size!
+	'''
+	def GetNormMatches(self, descriptors :list[ImageDescription]) -> list[ImageDescription]:
+		valid_matches = []
+		for source in descriptors:
+			source.errorL2 = cv2.norm( source.sourceImage, self.sourceImage, cv2.NORM_L2 )
+			source.similarity = 1 - source.errorL2 / ( source.imgWidth * source.imgHeight )
+			valid_matches.append( source )
+
+		valid_matches.sort(key=lambda desc: desc.HistFitness ,reverse=True)
+		return valid_matches
+
+	def GetTemplateMatches(self, descriptors :list[ImageDescription]) -> list[ImageDescription]:
+		valid_matches = []
+		for source in descriptors:
+			source.errorL2 = cv2.norm( source.sourceImage, self.sourceImage, cv2.NORM_L2 )
+			source.similarity = 1 - source.errorL2 / ( source.imgWidth * source.imgHeight )
+			valid_matches.append( source )
+
+		valid_matches.sort(key=lambda desc: desc.HistFitness ,reverse=True)
+		return valid_matches
+
+def union(a,b):
+	x = min(a[0], b[0])
+	y = min(a[1], b[1])
+	w = max(a[0]+a[2], b[0]+b[2]) - x
+	h = max(a[1]+a[3], b[1]+b[3]) - y
+	return (x, y, w, h)
+
+def intersection(a,b):
+	x = max(a[0], b[0])
+	y = max(a[1], b[1])
+	w = min(a[0]+a[2], b[0]+b[2]) - x
+	h = min(a[1]+a[3], b[1]+b[3]) - y
+	if w<0 or h<0: return False, (0,0,0,0)
+	return True, (x, y, w, h)
+
+def hasIntersection(a,b):
+	x = max(a[0], b[0])
+	y = max(a[1], b[1])
+	w = min(a[0]+a[2], b[0]+b[2]) - x
+	h = min(a[1]+a[3], b[1]+b[3]) - y
+	if w<0 or h<0: return False
+	return True
+
+def scaleRect(a, target):
+	(x, y, w, h) = a
+	x -= (int)((target[0] - w)/2)
+	y -= (int)((target[1] - h)/2)
+	w += (int)(target[0] - w)
+	h += (int)(target[1] - h)
+
+	return (x, y, w, h)
+
+def pointDistance(a,b):
+	return math.hypot(a[0] - b[0], a[1] - b[1])
